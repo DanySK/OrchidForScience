22aac897881feb1c154d64874b3c08493959fc86
Danilo Pianini <danilo.pianini@unibo.it>
2021-06-07 16:42:57 +0200

    Pick the Jacoco version externally

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ repositories {
     jcenter()
 }
 
+val additionalTools: Configuration by configurations.creating
+
 dependencies {
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:_")
     implementation(kotlin("stdlib-jdk8"))
@@ -28,6 +30,7 @@ dependencies {
     implementation("org.danilopianini:khttp:_")
     testImplementation("io.kotest:kotest-runner-junit5:_")
     testImplementation("io.kotest:kotest-assertions-core-jvm:_")
+    additionalTools("org.jacoco:org.jacoco.core:_")
 }
 
 tasks.withType<KotlinCompile> {
@@ -41,6 +44,13 @@ tasks.test {
         showStandardStreams = true
         exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
     }
+}
+
+jacoco {
+    toolVersion = additionalTools.resolvedConfiguration.resolvedArtifacts
+        .find { "jacoco" in it.moduleVersion.id.name }
+        ?.moduleVersion?.id?.version
+        ?: toolVersion
 }
 
 tasks.jacocoTestReport {
@@ -82,6 +92,7 @@ publishOnCentral {
         password = mavenCentral().password()
     }
 }
+
 publishing {
     publications {
         withType<MavenPublication> {
